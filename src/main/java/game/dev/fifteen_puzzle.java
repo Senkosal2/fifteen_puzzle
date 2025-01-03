0531aa1e37bcb464316d693e1ec89d62fa7ad4d4
Senkosal2 <senkosalker2@gmail.com>
2025-01-04 02:39:40 +0700

    Update fifteen_puzzle.java

--- src/main/java/game/dev/fifteen_puzzle.java
+++ src/main/java/game/dev/fifteen_puzzle.java
@@ -1,11 +1,11 @@
 package game.dev;
 
+import java.util.Random;
 import java.util.Scanner;
-
-public class fifteen_puzzle {
+class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        // char movement[] = {'a', 'w', 's', 'd'};
+        
         boolean canMoveDown = false, 
         canMoveUp = false, 
         canMoveLeft = false, 
@@ -15,27 +15,28 @@ public class fifteen_puzzle {
         boolean isWin = false;
         int count = 0;
 
-        
-        int num[][] = {
-            {1, 2, 3, 4},
-            {5, 6, 7, 8},
-            {9, 10, 11, 12},
-            {13, 14, 0, 15}
-        };
+        // int num[][] = {
+        //     {1, 2, 3, 4},
+        //     {5, 6, 7, 8},
+        //     {9, 10, 11, 12},
+        //     {13, 14, 0, 15}
+        // };
+        int[][] num = Randomize();
         
         while (true) { 
             // clear console
-            Clear();
+            // Clear();
             count = 0;
 
             // display the box
             for (int[] row : num) {
                 for (int col : row) {
-                    // System.out.println("#####");
                     System.out.print((col == 0 ? "" : col) + "\t");
                 }
                 System.out.println();
             }
+            
+            // win condition
             for (int[] row : num) {
                 for (int col : row) {
                     count++;
@@ -65,26 +66,13 @@ public class fifteen_puzzle {
                 if (isFound) break;
             }
 
-            // System.out.println("x: " + x + "\ny: " + y);
-
-
             // check for possible movement
             canMoveLeft = x - 1 >= 0 ? num[y][x-1] == 0 : false;
             canMoveRight = x + 1 < num[x].length ? num[y][x+1] == 0 : false;
             canMoveDown = y + 1 < num.length ? num[y+1][x] == 0 : false;
             canMoveUp = y - 1 >= 0 ? num[y-1][x] == 0 : false;
 
-            // display possible movement
-            // if (canMoveUp) System.out.println("W: Move Up");
-            // if (canMoveDown) System.out.println("S: Move Down");
-            // if (canMoveLeft) System.out.println("A: Move Left");
-            // if (canMoveRight) System.out.println("D: Move Right");
-            
-            // System.out.print("Movement: ");
-            // char selected_movement = input.next().charAt(0);
-
             // move right
-            // if (selected_movement == 'd' && canMoveRight) {
             if (canMoveRight) {
                 // swap value at index
                 int temp = num[y][x+1];
@@ -92,7 +80,6 @@ public class fifteen_puzzle {
                 num[y][x] = temp;
             }
             // move left
-            // if (selected_movement == 'a' && canMoveLeft) {
             if (canMoveLeft) {
                 // swap value at index
                 int temp = num[y][x-1];
@@ -100,7 +87,6 @@ public class fifteen_puzzle {
                 num[y][x] = temp;
             }
             // move up
-            // if (selected_movement == 'w' && canMoveUp) {
             if (canMoveUp) {
                 // swap value at index
                 int temp = num[y-1][x];
@@ -108,7 +94,6 @@ public class fifteen_puzzle {
                 num[y][x] = temp;
             }
             // move down
-            // if (selected_movement == 's' && canMoveDown) {
             if (canMoveDown) {
                 // swap value at index
                 int temp = num[y+1][x];
@@ -117,23 +102,39 @@ public class fifteen_puzzle {
             }
             isFound = false;
         }
+    }
+    
+    static int[][] Randomize() {
+        int[] array = new int[16];
+        int[][] numbers = new int[4][4];
+        int count = 0;
+        
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
 
-        // System.out.println("Up: " + canMoveUp);
-        // System.out.println("Down: " + canMoveDown);
-        // System.out.println("Right: " + canMoveRight);
-        // System.out.println("Left: " + canMoveLeft);
+        Random random = new Random();
+        for (int i = array.length - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers[i].length; j++) {
+                numbers[i][j] = array[count];
+                count++;
+            }
+        }
 
-        // for (int[] row : num) {
-        //     for (int col : row) {
-        //         System.out.print(col + "\t");
+        // for (int[] num : numbers) {
+        //     for (int n : num) {
+        //         System.out.print((n == 0 ? "": n) + "\t");
         //     }
         //     System.out.println();
         // }
-    }
-
-    static void Clear() {
-        System.out.println("\033[H\033[2J");
-        System.out.flush();
-        // System.out.println("Cleared!");
+        
+        return numbers;
     }
 }
